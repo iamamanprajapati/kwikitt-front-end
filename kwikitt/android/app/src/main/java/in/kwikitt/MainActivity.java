4ed32761e338003850ce98001e9a49d3b041a78d
aman kumar <spn.amankumar@gmail.com>
2020-06-27 19:52:56 +0530

    app version and packagne name fixed

--- kwikitt/android/app/src/main/java/in/kwikitt/MainActivity.java
+++ kwikitt/android/app/src/main/java/in/kwikitt/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kwikitt;
+package in.kwikitt;
 
 import com.facebook.react.ReactActivity;
 
--- kwikitt/android/app/src/main/java/in/kwikitt/MainApplication.java
+++ kwikitt/android/app/src/main/java/in/kwikitt/MainApplication.java
@@ -1,4 +1,4 @@
-package com.kwikitt;
+package in.kwikitt;
 
 import android.app.Application;
 import android.content.Context;
